091d907f1d5acea93138c6aeabb47c20e0936278
Harshal Pantawane <hmpantawane2214@gmail.com>
2026-08-08 17:11:20 +0530

    practice

--- practice/Part 2/Exercise.py
+++ practice/Part 2/Exercise.py
@@ -8,6 +8,40 @@
 
 # 2 WAP to fid occurrence of "$" in an string
 
-str = "hii $ my name is $ harshal $ i am boy"
+# str = "hii $ my name is $ harshal $ i am boy"
 
-print(str.count("$"))
+# print(str.count("$"))
+
+
+
+# 3 WAP to check if a number entered by the user is odd or even
+# num = int(input("Enter no: "))
+
+# if(num % 2 == 0):
+#     print("Number is Even")
+# else:
+#     print("Number is Odd")    
+   
+   
+    
+# 4 WAP to find the greatest of 3 number entered by the user.
+
+# num1 = int(input("Enter first num: "))   
+# num2 = int(input("Enter second num: "))
+# num3 = int(input("Enter third num: "))
+
+# if((num1 > num2) and (num1 > num3)):
+#     print("num1 is greatest num", num1)
+# elif(num2 > num3):
+#     print("num2 is greater num", num2)
+# else:
+#     print("num3 is greater num", num3)  
+
+
+# 5 WAP to chekc if a number is a multiple of 7 or not.
+num = int(input("Enter your no: "))      
+
+if(num % 7 == 0):
+    print("the num is divided by 7:", num)
+else:
+    print("Not divided by 7:", num)    
