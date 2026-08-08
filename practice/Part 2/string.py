18c1aea80bcd3d152c687537a5082b398c97dc9b
Harshal Pantawane <hmpantawane2214@gmail.com>
2026-08-08 13:25:03 +0530

    part 2

---
+++ practice/Part 2/string.py
@@ -0,0 +1,61 @@
+# str1 = "Hi my name is harshal."      # mostly used 
+# str2 = 'How are you?'
+# str3 = """i ma doing good."""
+
+# str4 = "Hi my name is harshal.\nsriya is my best friend." # no space and \n sriya  = space
+
+# print(str4)
+
+
+# str1 = "harshal"
+# str2 = "sriya"
+
+# print(str1+str2)
+# #####################################################
+
+# #length
+
+# str = "Sriya is my love"  # space will count
+# print(len(str))
+
+###################################
+# indexing
+# positive index
+# a s d f g h 
+# 0 1 2 3 4 5 
+
+# negative index
+# a  s  d  f  g  g 
+# -6 -5 -4 -3 -2 -1
+
+
+
+# str = "harshalsriya"  
+
+# print(str[2])
+
+# str[3] = "a"   # we cant modify string
+# print(str)
+
+#########################################
+
+# slicing  menas accessing part of string
+
+# positive index
+
+# str = "abcdefghijklmnopqrstuvwxyz"
+# str[1:5]        # ending idx is not included
+# str[ :4]        # start from 0
+# str[1: ]  or str[2:len(str)]    # upto ending
+
+# print(str[1:4])
+# print(str[ :4])
+# print(str[3: ])
+
+# negative index
+# negative index
+# a  s  d  f  g  g  
+# -6 -5 -4 -3 -2 -1 
+
+str = "harshalsriya"
+print(str[-5:-1])
